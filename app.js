9ac76041748f9154eeee6efa61643a191f46641e
Yulai Shen <shenyulai@gmail.com>
2012-04-21 14:39:57 -0400

    init commit

---
+++ app.js
@@ -0,0 +1,85 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , mongoose  = require('mongoose')
+  , Schema = mongoose.Schema
+  , mongooseAuth  = require('mongoose-auth')
+  , routes = require('./routes');
+
+var UserSchema = new Schema({})
+  , User;
+
+UserSchema.plugin(mongooseAuth, {
+    everymodule: {
+      everyauth: {
+          User: function () {
+            return User;
+          }
+      }
+    }
+  , password: {
+        loginWith: 'email'
+      , extraParams: {
+            name: {
+                first: String
+              , last: String
+            }
+        }
+      , everyauth: {
+            getLoginPath: '/login'
+          , postLoginPath: '/login'
+          , loginView: 'login.jade'
+          , getRegisterPath: '/register'
+          , postRegisterPath: '/register'
+          , registerView: 'register.jade'
+          , loginSuccessRedirect: '/tasks'
+          , registerSuccessRedirect: '/tasks'
+        }
+    }
+});
+
+mongoose.model('User', UserSchema);
+
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/taskboard_dev');
+
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'esoognom' }));
+  app.use(mongooseAuth.middleware());
+  //app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+
+app.get('/', routes.index);
+
+app.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+
+app.listen(3000, function(){
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
