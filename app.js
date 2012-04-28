f5f8406447dff05c99824c2264a192ef0b3ebab0
Yulai Shen <shenyulai@gmail.com>
2012-04-28 03:44:27 -0400

    port num uses env.PORT when possible

--- app.js
+++ app.js
@@ -205,6 +205,7 @@ app.get('/help', function (req, res) {
 
 mongooseAuth.helpExpress(app);
 
-app.listen(3000, function(){
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
